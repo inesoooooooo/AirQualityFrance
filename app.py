5437948950a7a0c9ec4f9eeec29238a655b24698
MyriamGit18 <myriam.1232003@gmail.com>
2025-11-21 16:11:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def update_map(annee, polluant):
 # -----------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
